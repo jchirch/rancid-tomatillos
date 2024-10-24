96f7086680bc34d16cd417187c6ed4732cda1b67
Candice Cirbo <thecirbos@icloud.com>
2024-10-24 10:45:54 -0600

    css styling

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,51 +1,52 @@
 // import Header from '../Header/Header';
-import './MovieDetails.css';
+import "./MovieDetails.css";
 // import movieDetails from '../data/movie_details.js'; // will be refactored with API data
 // import MoviePoster from '../MoviePoster/MoviePoster.js';
 // import { useParams } from 'react';
-import homePic from '../icons/green_home.png'
-import { useState, useEffect } from 'react';
-
-
-
+import homePic from "../icons/green_home.png";
+import { useState, useEffect } from "react";
 
 function MovieDetails({ selectedMovie, onHomeClick }) {
-  console.log("id", selectedMovie)
+  console.log("id", selectedMovie);
   const [movieDetails, setMovieDetails] = useState({});
 
   function getMovieDetails() {
-    console.log("tacos")
-    fetch(`https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${selectedMovie}`)
-     .then(response => response.json())
-     .then(data => {
-      console.log("data", data)
-      setMovieDetails(data)
-     } )
-     .catch(error => console.error("Error fetching movies:", error));
+    console.log("tacos");
+    fetch(
+      `https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${selectedMovie}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data", data);
+        setMovieDetails(data);
+      })
+      .catch((error) => console.error("Error fetching movies:", error));
   }
-  
-  useEffect(() => {
-    getMovieDetails()
-    }, []);
 
-    console.log("Movie Details:", movieDetails);
+  useEffect(() => {
+    getMovieDetails();
+  }, []);
+
+  console.log("Movie Details:", movieDetails);
   return (
-    <section className='MovieDetails'>
+    <section className="MovieDetails">
       {/* <Header/> */}
-      <div className='home-button'>
+      <div className="home-button">
         <button onClick={onHomeClick}>
           <img src={homePic} alt="home button" />
         </button>
       </div>
-      <img src={movieDetails.backdrop_path} alt={`Poster for ${movieDetails.title} `} />
-      <div className='detail-section'>
+      <div className="movie-image">
+        <img
+          src={movieDetails.backdrop_path}
+          alt={`Poster for ${movieDetails.title} `}
+        />
+      </div>
+      <div className="detail-section">
         <h2>{movieDetails.title}</h2>
-        <div className='genre-box'>
-        
-          {movieDetails.genre_ids && movieDetails.genre_ids.map(genre => (
-            <ul key={genre}>{genre}</ul>
-          ))} 
-          
+        <div className="genre-box">
+          {movieDetails.genre_ids &&
+            movieDetails.genre_ids.map((genre) => <ul key={genre}>{genre}</ul>)}
         </div>
         <p>{movieDetails.overview}</p>
       </div>
